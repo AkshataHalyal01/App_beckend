e882dbec7b2c28dae27d57e58c735745e80b0f53
AkshataHalyal01 <81458420+AkshataHalyal01@users.noreply.github.com>
2021-03-29 17:35:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,24 +1,8 @@
-if [ $# -eq 2 ]; then
-username=$1
-pass=$2
-
-	# Check if user already exists.
-	grep -q "$username" /etc/passwd
-	if [ $? -eq $SUCCESS ] 
-	then	
-	echo "User $username does already exist."
-  	echo "please chose another username."
-	exit $E_USEREXISTS
-	fi  
-
-
-	useradd -p `mkpasswd "$pass"` -d /home/"$username" -m -g users -s /bin/bash "$username"
-
-	echo "the account is setup"
-
+#!/bin/bash
+file="/etc/hosts"
+if [ -f "$file" ]
+then
+	echo "$file found."
 else
-        echo  " this programm needs 2 arguments you have given $# "
-        echo  " you have to call the script $0 username and the pass "
+	echo "$file not found."
 fi
-
-exit 0
