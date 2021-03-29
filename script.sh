122546b62836d683001140eb146b8816047f07b4
AkshataHalyal01 <81458420+AkshataHalyal01@users.noreply.github.com>
2021-03-29 16:42:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,24 @@
-a=$1
-b=$2
-echo "Hello $a!!"
-echo "changing $b"
-echo "pushing from qa"
-echo "nothing is working"
-echo "somtiming is working"
+if [ $# -eq 2 ]; then
+username=$1
+pass=$2
+
+	# Check if user already exists.
+	grep -q "$username" /etc/passwd
+	if [ $? -eq $SUCCESS ] 
+	then	
+	echo "User $username does already exist."
+  	echo "please chose another username."
+	exit $E_USEREXISTS
+	fi  
+
+
+	useradd -p `mkpasswd "$pass"` -d /home/"$username" -m -g users -s /bin/bash "$username"
+
+	echo "the account is setup"
+
+else
+        echo  " this programm needs 2 arguments you have given $# "
+        echo  " you have to call the script $0 username and the pass "
+fi
+
+exit 0
